ee9c4890437ee843b64bf4a745326c60922cf418
Moamen <Moamen.1992@hotmail.com>
2023-09-01 12:27:22 +0300

    image cache

---
+++ app/src/main/java/com/kryptonictest/di/AppsGlideModule.kt
@@ -0,0 +1,8 @@
+package com.kryptonictest.di
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppsGlideModule : AppGlideModule() {
+}
